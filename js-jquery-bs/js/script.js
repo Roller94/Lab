ffe772885b438a85739b1b936b439b09a8a52323
Roller Zúñiga Solano <razs94@hotmail.com>
2017-03-27 20:00:12 -0600

    Version 1.2
    Cambios

--- js-jquery-bs/js/script.js
+++ js-jquery-bs/js/script.js
@@ -2,30 +2,68 @@ $(document).ready(function() {
 
   	$("#btn_calcular_edad").click(function() {
 
-  		var texto = $("#name").val();
-  		var estilo = $("#fecha_nacimiento").val();
+  		var texto = $("#name").val() + " " + $("#fecha_nacimiento").val();
+  		var estilo = "classRed";
 
-	  	var obj = new mostrarResultados("mensaje privado", "mensaje público");
-		obj.metodoPublico();
+	  	var obj = new mostrarResultados(texto, estilo);
+		obj.presentarse();
 
 	});
 
 });
 
 
-function mostrarResultados(msjPrivado, msjPublico) {
+function mostrarResultados(texto, estilo) {
  
-    var propiedadPrivada = msjPrivado;
-    var propiedadPublica = msjPublico;
+    var propiedadPrivada = estilo;
+    var propiedadPublica = texto;
 
-    var metodoPrivado = function () {
-        alert(propiedadPrivada);
-        alert(propiedadPublica);
+    var calcularEdad = function () {
+        alert(estilo);
+        alert(texto);
     };
 
-    this.metodoPublico = function () {
-        metodoPrivado();
+    this.presentarse = function () {
+        calcularEdad();
     };
 }
 
 
+/*
+
+link: http://www.variablenotfound.com/2011/12/funciones-constructoras-en-javascript.html
+
+function Cliente(nombre, fecha, direccion) {
+ 
+        //Propiedades privadas
+        var edad;
+ 
+        //Métodos privados
+        var calcularEdad = function() {
+            var actual = new Date().getYear();
+            var nacimiento = fecha.getYear();
+ 
+            if (actual <= nacimiento)
+                edad = "Error: no se ha podido calcular";
+            else
+                edad = actual - nacimiento;
+        };
+ 
+        //Propiedades públicas
+        this._nombre = nombre;
+        this._fechaNacimiento = fecha;
+        this._edad = edad;
+        this._direccion = direccion;
+ 
+        //Métodos públicos
+        this._presentarse = function() {
+            calcularEdad();
+ 
+            document.write(
+                "Hola, mi nombre es " + this._nombre + 
+                " y tengo " + this._edad + " años."
+            );
+        };
+    }
+
+ */
